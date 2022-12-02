42855918d264aa3fa77f77c29898c26f9f32db3c
gunesevitan <gunesevitan@gmail.com>
2022-12-02 10:19:52 +0300

    cf and mf mdoels

--- src/matrix_factorization/torch_modules.py
+++ src/matrix_factorization/torch_modules.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 
 
@@ -9,36 +8,31 @@ class CollaborativeFiltering(nn.Module):
         super(CollaborativeFiltering, self).__init__()
 
         self.embeddings = nn.Embedding(num_embeddings=n_embeddings, embedding_dim=n_factors, sparse=sparse)
-        self.bias = nn.Embedding(num_embeddings=n_embeddings, embedding_dim=1, sparse=sparse)
         self.dropout = nn.Dropout(p=dropout_probability) if dropout_probability > 0 else nn.Identity()
 
     def forward(self, x1, x2):
 
-        bias = self.bias(x1) + self.bias(x2)
         x1 = self.embeddings(x1)
         x2 = self.embeddings(x2)
-        outputs = (self.dropout(x1) * self.dropout(x2)).sum(dim=1) + bias
+        outputs = (self.dropout(x1) * self.dropout(x2)).sum(dim=-1)
 
         return outputs
 
 
 class MatrixFactorization(nn.Module):
 
-    def __init__(self, n_sessions, n_aids, embedding_dim, sparse=False, dropout_probability=0):
+    def __init__(self, n_sessions, n_aids, n_factors, sparse=False, dropout_probability=0):
 
         super(MatrixFactorization, self).__init__()
 
-        self.session_embeddings = nn.Embedding(num_embeddings=n_sessions, embedding_dim=embedding_dim, sparse=sparse)
-        self.aid_embeddings = nn.Embedding(num_embeddings=n_aids, embedding_dim=embedding_dim, sparse=sparse)
-        self.session_bias = nn.Embedding(num_embeddings=n_sessions, embedding_dim=1, sparse=sparse)
-        self.aid_bias = nn.Embedding(num_embeddings=n_aids, embedding_dim=1, sparse=sparse)
-        self.dropout = nn.Dropout(p=dropout_probability)
+        self.session_embeddings = nn.Embedding(num_embeddings=n_sessions, embedding_dim=n_factors, sparse=sparse)
+        self.aid_embeddings = nn.Embedding(num_embeddings=n_aids, embedding_dim=n_factors, sparse=sparse)
+        self.dropout = nn.Dropout(p=dropout_probability) if dropout_probability > 0 else nn.Identity()
 
     def forward(self, sessions, aids):
 
-        sessions_weight = torch.squeeze(self.session_embeddings(sessions), dim=1)
-        aids_weight = torch.squeeze(self.aid_embeddings(aids), dim=1)
-        bias = torch.squeeze(self.session_bias(sessions) + self.aid_bias(aids), dim=1)
-        outputs = (self.dropout(sessions_weight) * self.dropout(aids_weight)).sum(dim=1, keepdim=True) + bias
+        sessions = self.session_embeddings(sessions)
+        aids = self.aid_embeddings(aids)
+        outputs = (self.dropout(sessions) * self.dropout(aids)).sum(dim=-1)
 
-        return outputs.squeeze(dim=1)
+        return outputs
