1b4983ccea8d68d30ca43100be2d9efb6a85fa88
gunesevitan <gunesevitan@gmail.com>
2023-01-17 15:53:58 +0300

    idk

--- src/metrics.py
+++ src/metrics.py
@@ -8,7 +8,7 @@ def click_recall(y_true, y_pred):
 
     Parameters
     ----------
-    y_true: int
+    y_true: array-like of shape (1)
         Ground-truth click aid
 
     y_pred: array-like of shape (n_aids) (1 <= n_aids <= 20)
@@ -20,7 +20,11 @@ def click_recall(y_true, y_pred):
         Recall calculated on ground-truth and predictions
     """
 
-    recall = int(y_true in y_pred)
+    if len(y_true) == 0:
+        recall = np.nan
+    else:
+        recall = int(y_true[0] in y_pred)
+
     return recall
 
 
@@ -48,6 +52,7 @@ def cart_order_recall(y_true, y_pred):
 
     tp = len(y_true.intersection(y_pred))
     fn = len(y_true - y_pred)
+
     try:
         recall = tp / min(20, (tp + fn))
     except ZeroDivisionError:
